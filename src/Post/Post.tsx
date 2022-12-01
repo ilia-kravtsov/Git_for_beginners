4449dc142ecd012c2d9c7f2b1d5af38bfbc84c63
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2022-12-01 21:19:48 +0400

    Add Post Test User components

---
+++ src/Post/Post.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Post = () => {
+    return (
+        <div>
+            <h2>Post_1</h2>
+            <h2>Post_2</h2>
+        </div>
+    )
+}
---
+++ src/User/User.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const User = () => {
+    return (
+        <div>
+            <h2>User_1</h2>
+            <h2>User_2</h2>
+        </div>
+    )
+}
