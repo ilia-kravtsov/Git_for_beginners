e0b5b75b1426fb039aa553841cf6886475c2a53c
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2022-12-03 20:12:37 +0400

    Adding some changes in User and Text for popup branch

--- src/Test/Text.tsx
+++ src/Test/Text.tsx
@@ -9,6 +9,7 @@ export const Test = () => {
                 <h2>Test_3</h2>
                 <h2>Test_4</h2>
                 <h2>Test_5</h2>
+                <h2>Test_6_Add_to_popup</h2>
             </div>
         </div>
     )
